4349c591cc815d765dd555be046fee40a001476b
Brady Marks <furiousgreencloud@gmail.com>
2013-08-09 20:06:32 -0700

    Inital Commit

---
+++ projects/blink/blink.py
@@ -0,0 +1,26 @@
+# IMPORTS
+from time import sleep
+import RPi.GPIO as GPIO
+
+# VARIABLES
+pinNo = 25
+     
+# SETUP
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+
+
+def main() :
+	# LOOP
+	while 1:
+		GPIO.output(pinNo, GPIO.HIGH)
+		print("hi")
+		sleep(2)
+		GPIO.output(pinNo, GPIO.LOW)
+		print("lo")
+		sleep(1)
+		
+main()
+
+
+GPIO.cleanup()
---
+++ projects/sendmail/sendmail.py
@@ -0,0 +1,16 @@
+import os
+
+def quote(s) :
+	return '"' + s.replace('"','\\"') + '"'
+	
+def shell(cmd) :
+	print("Doing: \n\t" + cmd)
+	os.system(cmd)
+
+def sendmail(subject, to, msg) :
+	shell('echo '+ quote(msg) +' | mail -s ' + quote(subject) + ' ' + quote(to))
+
+
+sendmail('In reguard to the matter of "me"',
+  "furiousgreencloud@gmail.com",
+  "You didn't ask before you took my favourite pencil")
