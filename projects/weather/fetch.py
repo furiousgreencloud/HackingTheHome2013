30abed698c18ff682da3101b63f92fb3a9da432b
Brady Marks <furiousgreencloud.com>
2013-08-14 07:58:01 -0700

    Added Vancouver Weather Fetcher

---
+++ projects/weather/fetch.py
@@ -0,0 +1,20 @@
+import urllib2
+import json
+
+
+def dump(thing) :
+	print(type(thing))
+	print(thing)
+
+def kelvin2celius(k) :
+	return k - 273.15
+
+print("Fetching Weather...");
+response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/weather?q=Vancouver&mode=json')
+decoded = json.load(response)
+#print(json.dumps(decoded, sort_keys=True, indent=2))
+print(decoded["name"] + " Weather: " + decoded["weather"][0]["description"])
+maxTemp = kelvin2celius(decoded["main"]["temp_max"])
+minTemp = kelvin2celius(decoded["main"]["temp_min"])
+print("Max Temp: " + str(maxTemp) + "C")
+print("Min Temp: " + str(minTemp) + "C")
